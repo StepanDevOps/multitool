eb066892e4d0caf495820009ee607cd326d27986
Stepan <stepanpeshkov101@gmail.com>
2026-04-05 18:08:31 +0300

    feat: добавлена поддержка Jetpack Compose и определены темы и типографика

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     id("com.google.devtools.ksp") version "2.3.4" apply false
     id("androidx.room") version room_version apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
